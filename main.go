0dba01e681668ba6641a294eae0088d2f27c4433
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2024-03-05 01:44:51 +0700

    Add .gitignore and update main.go

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Worlds!")
+	fmt.Println(Calculate(4))
+}
+
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
